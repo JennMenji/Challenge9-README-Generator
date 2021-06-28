72dcea4b661f06c8fe4d76e225659c25677ebe59
Menji <jennifer.menjivar18@gmail.com>
2021-06-28 01:09:58 -0400

    completed basic general func to create readme

--- Develop/dist/README.md
+++ Develop/dist/README.md
@@ -1,23 +1,9 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `
-    # ${data.title}
+    # Miscief Managed
 
     ## Description 
 
-    ${data.description}
+    This means it worked
 
     ## Table of Contents (Optional)
 
@@ -87,7 +73,3 @@ function generateMarkdown(data) {
 
     Go the extra mile and write tests for your application. Then provide examples on how to run them.
 
-`;
-}
-
-module.exports = generateMarkdown;
--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,14 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
+const Choices = require("inquirer/lib/objects/choices");
+const generatePage = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
   {
     type: "input",
-    name: "projectTitle",
+    name: "title",
     message: "What is the Title of your Project?",
   },
   {
@@ -14,68 +16,54 @@ const questions = [
     name: "description",
     message: "Please enter a description of your project.",
   },
-  //   {
-  //     type: "input",
-  //     name: "installationInstruc",
-  //     message:
-  //       "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
-  //   },
-  //   {
-  //     type: "input",
-  //     name: "usuageInfo",
-  //     message:
-  //       "Provide instructions and examples for use. Include screenshots as needed.",
-  //   },
+  {
+    type: "input",
+    name: "installation",
+    message:
+      "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+  },
+  {
+    type: "filter",
+    name: "usage",
+    message:
+      "Provide instructions and examples for use. Include screenshots as needed.",
+  },
   //   {
   //     type: "input",
   //     name: "contributionGuidelines",
   //     message: "rfrf",
   //   },
+  {
+    type: "confirm",
+    name: "contentsConfrim",
+    message:
+      "Would you like to include a table of contents, to make it easy for users to find what they need? (Optional)",
+    default: true,
+  },
 ];
 
 // TODO: Create a function to write README file
-// function writeToFile(readmeInput) {
-
-// }
-
 const writeToFile = (readmeInput) => {
-  return new Promise((resolve, reject) => {
-    fs.writeFile("./dist/index.html", readmeInput, (err) => {
-      //   if there is an error, reject the Promise and send the error to the Promise's ".catch()" method
-      if (err) {
-        reject(err);
-        // retrun out of the function here to make sure the Promise doesn't accidentally execute the resolve function as well
-        return;
-      }
+  fs.writeFile("./dist/README.md", generatePage(readmeInput), (err) => {
+    if (err) {
+      return console.log(err);
+    }
 
-      //   if everything went well, resolve the Promise and send the successful data to the ".then()" method
-      resolve({
-        ok: true,
-        message: "File created!",
-      });
-    });
+    console.log("File created!");
   });
 };
 
 // TODO: Create a function to initialize app
 function init() {
-  return inquirer.prompt(questions);
+  return inquirer
+    .prompt(questions)
+    .then((data) => {
+      return writeToFile(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 // Function call to initialize app
-init()
-  .then((data) => {
-    return writeToFile(data);
-    //     fs.writeFile("hello.txt", data, (err) => {
-    //       if (err) {
-    //         return console.log("wtf");
-    //       }
-    //       console.log("you did it bitch!");
-    //     });
-    //   })
-    //   .catch((err) => {
-    //     console.log("err");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+init();
